3b18791976d94cdab28b4673d6ccedb21e5c1f83
pedroesri97-dotcom <pedroesri97@gmail.com>
2026-08-25 12:29:49 -0500

    Delete plantilla_capas directory

--- plantilla_capas/src/runtime/translations/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  _widgetLabel: 'Plantilla Capas por Componente',
-  title: 'Capas por',
-  titleHighlight: 'componente',
-  selectMapWarning: 'Por favor, seleccione un widget de Mapa en la configuración del widget.'
-};
