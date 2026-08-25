cbc4d20be5d2cfcd737ac574fb720a6074ee6102
pedroesri97-dotcom <pedroesri97@gmail.com>
2026-08-25 12:19:51 -0500

    Delete plantilla_capas 1/plantilla_capas directory

--- plantilla_capas 1/plantilla_capas/src/config.ts
+++
@@ -1,9 +0,0 @@
-import { ImmutableObject } from 'jimu-core';
-
-export interface Config {
-  title?: string;
-  titleHighlight?: string;
-  defaultExpandedGroup?: string;
-}
-
-export type IMConfig = ImmutableObject<Config>;
