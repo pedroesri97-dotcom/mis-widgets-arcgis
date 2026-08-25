1eaf2728d7d38b2e960e4a9efdac60364532c5a4
pedroesri97-dotcom <pedroesri97@gmail.com>
2026-08-25 12:20:37 -0500

    Add files via upload

---
+++ plantilla_capas/src/runtime/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  _widgetLabel: 'Plantilla Capas por Componente',
+  title: 'Capas por',
+  titleHighlight: 'componente',
+  selectMapWarning: 'Por favor, seleccione un widget de Mapa en la configuración del widget.'
+};
