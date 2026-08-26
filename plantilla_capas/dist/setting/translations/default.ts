48a52af812c2ad86fc46b7681da0aeb89043a88b
pedroesri97-dotcom <pedroesri97@gmail.com>
2026-08-26 11:05:33 -0500

    Delete plantilla_capas directory

--- plantilla_capas/dist/setting/translations/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  selectMapWidget: 'Seleccionar widget de Mapa',
-  settings: 'Configuración',
-  titleLabel: 'Título principal',
-  titleHighlightLabel: 'Texto resaltado (Cyan)'
-};
